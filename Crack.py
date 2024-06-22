cc8c234a95570688c8e68977f215bec7e06dca8b
TaliGanda <145276291+TaliGanda@users.noreply.github.com>
2024-06-22 22:37:01 +0700

    Add files via upload

---
+++ Crack.py
@@ -0,0 +1,24 @@
+print("Welcome To Devil Dox")
+print("Follow the policies we have implemented")
+
+input("Id Tele|>")
+input("User Tele|>")
+
+ Dox():
+	clear()
+	print("""
+	
+{fg(70)}                     .::.  ╔══════════════════════════════════════════════════════════╗═╗
+{fg(71)}                  .:'  .:  ║ ━ ━ ━ ━ ━ ━ ━ WELCOME TO Deviler-Dx.global ━ ━ ━ ━ ━ ━ ━ ║ ║
+{fg(72)}        ,MMM8&&&.:'   .:'  ╠═══╦═════════╦════════════════════════════════════════════╣ ║
+{fg(73)}       MMMMM88&&&&  .:'    ║ 1 ║   STRESS║ Make the target data ( small target )      ║ ║
+{fg(74)}      MMMMM88&&&&&&:'      ╠═══╬═════════╬════════════════════════════════════════════╣ ║
+{fg(75)}      MMMMM88&&&&&&        ║ 2 ║   DOX   ║ Transmission Control Protocol Crack password ║ ║
+{fg(45)}    .:MMMMM88&&&&&&        ╠═══╬═════════╬════════════════════════════════════════════╣ ║
+{fg(44)}  .:'  MMMMM88&&&&         ║ 3 ║   DOX2  ║ User Telegram Protocol Crack Doxing        ║ ║
+{fg(43)}.:'   .:'MMM8&&&'          ╠═══╬═════════╬════════════════════════════════════════════╣ ║
+{fg(42)}:'  .:'                    ║ 4 ║   ADS   ║ BUY Hit   Dx --> xxxxxxxxxxxxxxxxxx        ║ ║
+{fg(41)}'::'                       ╠═══╩═════════╩════════════════════════════════════════════╣ ║
+{fg(40)}MADE BY XxxxxXxxxXxxxXXXXX ║ ━ ━ ━ ━ ━ ━ ━WELCOME TO Deviler-Dx.global ━ ━ ━ ━ ━ ━ ━ ║ ║
+{fg(34)}                           ╚══════════════════════════════════════════════════════════╝═╝
+""")
